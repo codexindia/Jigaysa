ff5a76843479724840e7e428ea0b1284c5ea0d6c
codexindia <sudiptobain8@gmail.com>
2026-06-30 18:19:29 +0530

    fix: Correct Django settings module name in WSGI configuration

--- Jigaysa/wsgi.py
+++ Jigaysa/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Jigayasa.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Jigaysa.settings')
 
 application = get_wsgi_application()
